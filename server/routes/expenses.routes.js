8de6910e0e1c9a1cce6bd54b323c0b6f8974e72e
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2022-09-22 15:20:09 -0500

    requests now go through middleware

--- server/routes/expenses.routes.js
+++ server/routes/expenses.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
 const expenses = require("../controllers/expenses.controller");
 const verifyToken = require("../../utils/verifyToken");
+
 router.get("/", expenses.getAllExpenses);
-router.get("/:id", expenses.getExpenses);
-router.post("/:id", expenses.addExpense);
-router.delete("/:userID/:expenseID", expenses.deleteExpense);
-router.patch("/:id", expenses.updateExpense);
+router.get("/:id", verifyToken, expenses.getExpenses);
+router.post("/:id", verifyToken, expenses.addExpense);
+router.delete("/:userID/:expenseID", verifyToken, expenses.deleteExpense);
+router.patch("/:id", verifyToken, expenses.updateExpense);
 
 module.exports = router;
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
 const user = require("../controllers/user.controller");
+const verifyToken = require("../../utils/verifyToken");
 
 router.get("/", user.getAllUsers);
 router.post("/", user.registerUser);
 router.post("/login", user.login);
-router.put("/updateBudget/:userID", user.updateBudget);
+router.put("/updateBudget/:userID", verifyToken, user.updateBudget);
 
 module.exports = router;
